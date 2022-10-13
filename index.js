e7097db8250d6c421b104908df62a97d8c8b3f04
Hu Kaixiang <maxhu787@gmail.com>
2022-10-13 21:37:08 +0800

    update

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ img.crossOrigin = "anonymous";
 img.src = "./test.jpg";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
+
+//const canvasWidth = canvas.width;
+//const canvasHeight = canvas.height;
+
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
+
+
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
 img.addEventListener("load", () => {
     ctx.drawImage(img, 0, 0);
     img.style.display = "none";
@@ -42,23 +51,41 @@ window.addEventListener('load', (event) => {
 
     let output = document.getElementById('output');
 
-    for (let i = 0; i < canvasWidth; i++) {
-        for (var j = 1; j < canvasHeight; j++) {
-        let x = i;
-        let y = j;
-        const pixel = ctx.getImageData(x, y, 1, 1);
-        const data = pixel.data;
+    /*for (let i = 0; i < canvasWidth; i = i + 10) {
+        for (var j = 1; j < canvasHeight; j = j + 10) {
+            let x = i;
+            let y = j;
+            const pixel = ctx.getImageData(x, y, 1, 1);
+            const data = pixel.data;
 
-        const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+            const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
 
-        let outputElem = document.createElement("div")
-        outputElem.classList.add("output-elem")
-        outputElem.textContent = "0";
-        /*output.innerHTML += "<p style='color:" + rgba + ";'>0</p>";*/
-        outputElem.style.background = rgba;
-        output.appendChild(outputElem);
-    };
+            let outputElem = document.createElement("div")
+            outputElem.classList.add("output-elem")
+            outputElem.textContent = "0";
+            outputElem.style.color = rgba;
+            output.appendChild(outputElem);
+        };
         let newLine = document.createElement("div");
-    output.appendChild(newLine);
-}
+        output.appendChild(newLine);
+    }*/
+    //
+    for (let i = 0; i < canvasHeight; i = i + 18) {
+        for (var j = 1; j < canvasWidth; j = j + 9) {
+            let x = j;
+            let y = i;
+            const pixel = ctx.getImageData(x, y, 1, 1);
+            const data = pixel.data;
+
+            const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+
+            let outputElem = document.createElement("div")
+            outputElem.classList.add("output-elem")
+            outputElem.textContent = "0";
+            outputElem.style.color = rgba;
+            output.appendChild(outputElem);
+        };
+        let newLine = document.createElement("div");
+        output.appendChild(newLine);
+    }
 })
