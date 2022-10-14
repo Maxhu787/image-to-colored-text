4068390c4b99650eb54fcd1ee25f60aedf8b07d9
Hu Kaixiang <maxhu787@gmail.com>
2022-10-14 21:35:22 +0800

    update

--- index.js
+++ index.js
@@ -11,34 +11,21 @@ const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight;
 
 
+//ctx.canvas.width = window.innerWidth;
+//ctx.canvas.height = window.innerHeight;
+
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+
 img.addEventListener("load", () => {
-    ctx.drawImage(img, 0, 0);
+    //ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img, -100, -100, img.width *  3, img.height * 3);
     img.style.display = "none";
 });
 const hoveredColor = document.getElementById("hovered-color");
 const selectedColor = document.getElementById("selected-color");
 
-function pick(event, destination) {
-    const bounding = canvas.getBoundingClientRect();
-    const x = event.clientX - bounding.left;
-    const y = event.clientY - bounding.top;
-    const pixel = ctx.getImageData(x, y, 1, 1);
-    const data = pixel.data;
-
-    console.log('x', x);
-    console.log('y', y);
-    const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
-    destination.style.background = rgba;
-    destination.textContent = rgba;
-
-    return rgba;
-}
-
-canvas.addEventListener("mousemove", (event) => pick(event, hoveredColor));
-canvas.addEventListener("click", (event) => pick(event, selectedColor));
 
 window.addEventListener('load', (event) => {
     let x = 0;
